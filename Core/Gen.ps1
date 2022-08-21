804196a3dce2bb8690593b2323c3c3be6ad70fb6
Jax <80020581+EnhancedJax@users.noreply.github.com>
2022-08-21 14:08:46 +0800

    Fixed scope

--- Core/Gen.ps1
+++ Core/Gen.ps1
@@ -1,28 +1,27 @@
-function Update {
-    $global:t = [int]$RmAPI.VariableStr('Total')
-    $global:skinsPath = $RmAPI.VariableStr('SKINSPATH')
-    $global:coreDataDir = "$global:skinsPath\..\CoreData\"
-    $global:file1 = "$global:coreDataDir"+"Keylaunch\Include.inc"
-    $global:file2 = "$global:coreDataDir"+"Keylaunch\Act.inc"
-    $global:file3 = "$global:coreDataDir"+"Keylaunch\Hotkeys.inc"
-    $global:file4 = "$global:coreDataDir"+"Keylaunch\Keylaunch.ahk"
-    $global:arrayName = New-Object System.Collections.Generic.List[System.Object]
-    $global:arrayAction = New-Object System.Collections.Generic.List[System.Object]
-    $global:arrayIcon = New-Object System.Collections.Generic.List[System.Object]
-    $global:arrayKey = New-Object System.Collections.Generic.List[System.Object]
-    $global:arrayKeyS = New-Object System.Collections.Generic.List[System.Object]
-    WriteToMemory
-}
-
 function WriteToMemory {
-    for ($i=1 ; $i -le $global:t ; $i++) {
-        $global:arrayName.Add($RmAPI.VariableStr("$($i)Name"))
-        $global:arrayAction.Add($RmAPI.VariableStr("$($i)Action"))
-        $global:arrayIcon.Add($RmAPI.VariableStr("$($i)Icon"))
-        $global:arrayKey.Add($RmAPI.VariableStr("Key$($i)"))
-        $global:arrayKeyS.Add($RmAPI.VariableStr("Key$($i)InString"))
+    for ($i=1 ; $i -le $t ; $i++) {
+        $arrayName.Add($RmAPI.VariableStr("$($i)Name"))
+        $arrayAction.Add($RmAPI.VariableStr("$($i)Action"))
+        $arrayIcon.Add($RmAPI.VariableStr("$($i)Icon"))
+        $arrayKey.Add($RmAPI.VariableStr("Key$($i)"))
+        $arrayKeyS.Add($RmAPI.VariableStr("Key$($i)InString"))
     }
 }
+
+$t = [int]$RmAPI.VariableStr('Total')
+$skinsPath = $RmAPI.VariableStr('SKINSPATH')
+$coreDataDir = "$skinsPath\..\CoreData\"
+$file1 = "$coreDataDir"+"Keylaunch\Include.inc"
+$file2 = "$coreDataDir"+"Keylaunch\Act.inc"
+$file3 = "$coreDataDir"+"Keylaunch\Hotkeys.inc"
+$file4 = "$coreDataDir"+"Keylaunch\Keylaunch.ahk"
+$arrayName = New-Object System.Collections.Generic.List[System.Object]
+$arrayAction = New-Object System.Collections.Generic.List[System.Object]
+$arrayIcon = New-Object System.Collections.Generic.List[System.Object]
+$arrayKey = New-Object System.Collections.Generic.List[System.Object]
+$arrayKeyS = New-Object System.Collections.Generic.List[System.Object]
+$RmAPI.Log($t)
+WriteToMemory
 
 function WriteAll {
     param(
@@ -38,7 +37,7 @@ function WriteAll {
     #                                 write include                                #
     # ---------------------------------------------------------------------------- #
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file1content += @"
+        $file1content += @"
 [$($i).Shape]
 Meter=Shape
 MeterStyle=Action.Shape:S
@@ -64,22 +63,22 @@ MeterStyle=Set.String:S | Action.Delete.StringIcon:S
     # ---------------------------------------------------------------------------- #
     #                                write variables                               #
     # ---------------------------------------------------------------------------- #
-    $global:file2content += @"
+    $file2content += @"
 [Variables]
 Total=$($arg1)
 
 "@
-    if ($arg1 -gt $global:t) {
-        $l = $global:t
+    if ($arg1 -gt $t) {
+        $l = $t
      } else {
         $l = $arg1
      }
      
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file2content += @"
-$($i)Name=$($global:arrayName[$i-1])
-$($i)Action=$($global:arrayAction[$i-1])
-$($i)Icon=$($global:arrayIcon[$i-1])
+        $file2content += @"
+$($i)Name=$($arrayName[$i-1])
+$($i)Action=$($arrayAction[$i-1])
+$($i)Icon=$($arrayIcon[$i-1])
 
 "@
     }
@@ -87,16 +86,16 @@ $($i)Icon=$($global:arrayIcon[$i-1])
     # ---------------------------------------------------------------------------- #
     #                              write ahk variables                             #
     # ---------------------------------------------------------------------------- #
-    $global:file3content += @"
+    $file3content += @"
 [Variables]
 RMPATH=$($RmAPI.VariableStr('PROGRAMPATH'))Rainmeter.exe
 
 "@
 
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file3content += @"
-Key$($i)=$($global:arrayKey[$i-1])
-Key$($i)InString=$($global:arrayKeyS[$i-1])
+        $file3content += @"
+Key$($i)=$($arrayKey[$i-1])
+Key$($i)InString=$($arrayKeyS[$i-1])
 
 "@
     }
@@ -104,7 +103,7 @@ Key$($i)InString=$($global:arrayKeyS[$i-1])
     # ---------------------------------------------------------------------------- #
     #                                   write ahk                                  #
     # ---------------------------------------------------------------------------- #
-    $global:file4content += @"
+    $file4content += @"
 #SingleInstance Force
 #Persistent
 Menu, Tray, Icon, Keylaunch.ico, 1
@@ -126,30 +125,30 @@ AHK_NOTIFYICON(wParam, lParam, uMsg, hWnd)
 
 "@
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file4content += @"
+        $file4content += @"
 IniRead, Key$($i), $file3, Variables, Key$($i)
 
 "@
     }
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file4content += @"
+        $file4content += @"
 Try Hotkey, %Key$($i)%, Action$($i)
 
 "@
     }
-    $global:file4content += @"
+    $file4content += @"
 Return
 
 "@
     for ($i=1 ; $i -le $arg1 ; $i++) {
-        $global:file4content += @"
+        $file4content += @"
 Action$($i):
     SendToReceiver($($i))
 Return
 
 "@
     }
-    $global:file4content += @"
+    $file4content += @"
 SendToReceiver(index)
 {
     IniRead, RainmeterPath, $file3, Variables, RMPATH
@@ -158,10 +157,10 @@ SendToReceiver(index)
 
 "@
 
-    $global:file1content | Out-File -FilePath $global:file1 -Encoding unicode
-    $global:file2content | Out-File -FilePath $global:file2 -Encoding unicode
-    $global:file3Content | Out-File -FilePath $global:file3 -Encoding unicode
-    $global:file4Content | Out-File -FilePath $global:file4 -Encoding unicode
+    $file1content | Out-File -FilePath $file1 -Encoding unicode
+    $file2content | Out-File -FilePath $file2 -Encoding unicode
+    $file3Content | Out-File -FilePath $file3 -Encoding unicode
+    $file4Content | Out-File -FilePath $file4 -Encoding unicode
 
     $RmAPI.Bang('[!UpdateMEasure Auto_Refresh:M][!Refresh]')
 }
@@ -211,23 +210,23 @@ function Set-IniContent($InputObject, $FilePath)
     $outFile = New-Item -ItemType file -Path $Filepath
     foreach ($i in $InputObject.keys)
     {
-        if (!($($InputObject[$i].GetType().Name) -eq “Hashtable”))
+        if (!($($InputObject[$i].GetType().Name) -eq â€œHashtableâ€))
         {
             #No Sections
-            Add-Content -Path $outFile -Value “$i=$($InputObject[$i])”
+            Add-Content -Path $outFile -Value â€œ$i=$($InputObject[$i])â€
         } else {
             #Sections
-            Add-Content -Path $outFile -Value “[$i]”
+            Add-Content -Path $outFile -Value â€œ[$i]â€
             Foreach ($j in ($InputObject[$i].keys | Sort-Object))
             {
-                if ($j -match “^Comment[\d]+”) {
-                    Add-Content -Path $outFile -Value “$($InputObject[$i][$j])”
+                if ($j -match â€œ^Comment[\d]+â€) {
+                    Add-Content -Path $outFile -Value â€œ$($InputObject[$i][$j])â€
                 } else {
-                    Add-Content -Path $outFile -Value “$j=$($InputObject[$i][$j])”
+                    Add-Content -Path $outFile -Value â€œ$j=$($InputObject[$i][$j])â€
                 }
 
             }
-            Add-Content -Path $outFile -Value “”
+            Add-Content -Path $outFile -Value â€œâ€
         }
     }
 }
@@ -244,15 +243,15 @@ function WriteTo {
         $param3
     )
     if ($PSBoundParameters.ContainsKey('param1')) {
-        $global:arrayName[$index-1] = $param1
+        $arrayName[$index-1] = $param1
     }
     if ($PSBoundParameters.ContainsKey('param2')) {
-        $global:arrayAction[$index-1] = $param2
+        $arrayAction[$index-1] = $param2
     }
     if ($PSBoundParameters.ContainsKey('param3')) {
-        $global:arrayIcon[$index-1] = $param3
+        $arrayIcon[$index-1] = $param3
     }
-    WriteAll -arg1 $global:t -arg2 ($global:t+1) -arg3 ($global:t+2)
+    WriteAll -arg1 $t -arg2 ($t+1) -arg3 ($t+2)
 
     $RmAPI.Bang('[!Refresh "#JaxCore\Accessories\GenericInteractionBox"]')
 }
@@ -268,25 +267,26 @@ function WriteHotkey {
     )
     
     # $Ini = Get-IniContent -filePath $file3
-    $global:arrayKeyS[$SecNum-1] = $hotkeyString
-    $global:arrayKey[$SecNum-1] = $hotkey
+    $arrayKeyS[$SecNum-1] = $hotkeyString
+    $arrayKey[$SecNum-1] = $hotkey
 
     Set-IniContent -ini $Ini -filePath $file3
-    WriteAll -arg1 $global:t -arg2 ($global:t+1) -arg3 ($global:t+2)
+    WriteAll -arg1 $t -arg2 ($t+1) -arg3 ($t+2)
 
     # $RmAPI.Bang('[!UpdateMeasure Auto_Refresh:M "#JaxCore\Main"][!Refresh "#JaxCore\Main"]')
 }
 
 function WriteAdd {
-    $global:t++
+    $t++
+    $RmAPI.Log($t)
 
-    $global:arrayName.Add("Blank action")
-    $global:arrayAction.Add("[!Log `"This is a blank action`"]")
-    $global:arrayIcon.Add("folder_png")
-    $global:arrayKey.Add("")
-    $global:arrayKeyS.Add("Edit Hotkey")
+    $arrayName.Add("Blank action")
+    $arrayAction.Add("[!Log `"This is a blank action`"]")
+    $arrayIcon.Add("folder_png")
+    $arrayKey.Add("")
+    $arrayKeyS.Add("Edit Hotkey")
     
-    WriteAll -arg1 $global:t -arg2 ($global:t+1) -arg3 ($global:t+2)
+    WriteAll -arg1 $t -arg2 ($t+1) -arg3 ($t+2)
 }
 
 function WriteRemove {
@@ -295,16 +295,16 @@ function WriteRemove {
         $startingIndex
     )
     
-    for ($i=$startingIndex;$i -le ($global:t-1);$i++) {
-        $RmAPI.Log("$($global:arrayName[$i-1]) -> $($global:arrayName[$i])")
-        $global:arrayName[$i-1] = $global:arrayName[$i]
-        $global:arrayAction[$i-1] = $global:arrayAction[$i]
-        $global:arrayIcon[$i-1] =  $global:arrayIcon[$i]
-        $global:arrayKey[$i-1] = $global:arrayKey[$i]
-        $global:arrayKeyS[$i-1] = $global:arrayKeyS[$i]
+    for ($i=$startingIndex;$i -le ($t-1);$i++) {
+        $RmAPI.Log("$($arrayName[$i-1]) -> $($arrayName[$i])")
+        $arrayName[$i-1] = $arrayName[$i]
+        $arrayAction[$i-1] = $arrayAction[$i]
+        $arrayIcon[$i-1] =  $arrayIcon[$i]
+        $arrayKey[$i-1] = $arrayKey[$i]
+        $arrayKeyS[$i-1] = $arrayKeyS[$i]
     }
-    $global:t = $global:t - 1
-    WriteAll -arg1 $global:t -arg2 ($global:t+1) -arg3 ($global:t+2)
+    $t = $t - 1
+    WriteAll -arg1 $t -arg2 ($t+1) -arg3 ($t+2)
     $RmAPI.Bang('[!Refresh]')
 }
 
@@ -319,8 +319,8 @@ function WriteSwap {
     $i1--
     $i2--
     
-    $RmAPI.Log("$($global:arrayName[$i1]) <-> $($global:arrayName[$i2])")
-    $global:arrayName[$i1], $global:arrayAction[$i1], $global:arrayIcon[$i1], $global:arrayKey[$i1], $global:arrayKeyS[$i1], $global:arrayName[$i2], $global:arrayAction[$i2], $global:arrayIcon[$i2], $global:arrayKey[$i2], $global:arrayKeyS[$i2] = $global:arrayName[$i2], $global:arrayAction[$i2], $global:arrayIcon[$i2], $global:arrayKey[$i2], $global:arrayKeyS[$i2], $global:arrayName[$i1], $global:arrayAction[$i1], $global:arrayIcon[$i1], $global:arrayKey[$i1], $global:arrayKeyS[$i1]
-    WriteAll -arg1 $global:t -arg2 ($global:t+1) -arg3 ($global:t+2)
+    $RmAPI.Log("$($arrayName[$i1]) <-> $($arrayName[$i2])")
+    $arrayName[$i1], $arrayAction[$i1], $arrayIcon[$i1], $arrayKey[$i1], $arrayKeyS[$i1], $arrayName[$i2], $arrayAction[$i2], $arrayIcon[$i2], $arrayKey[$i2], $arrayKeyS[$i2] = $arrayName[$i2], $arrayAction[$i2], $arrayIcon[$i2], $arrayKey[$i2], $arrayKeyS[$i2], $arrayName[$i1], $arrayAction[$i1], $arrayIcon[$i1], $arrayKey[$i1], $arrayKeyS[$i1]
+    WriteAll -arg1 $t -arg2 ($t+1) -arg3 ($t+2)
     $RmAPI.Bang('[!Refresh]')
 }
